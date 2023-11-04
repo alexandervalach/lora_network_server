e5fa1e18626d41443a96d65d2bb59ac1a145a200
Alexander Valach <25947314+alexandervalach@users.noreply.github.com>
2023-11-04 11:51:13 +0100

    Improved install_service.sh

--- bin/install_service.sh
+++ bin/install_service.sh
@@ -1,41 +1,57 @@
-#!/bin/sh
+#!/bin/bash
+
+# Default service name and script location
+service_name="lones"
+script_location="$(pwd)/network-server.sh"
 
 # Check root user
-if [ `id -u` != 0 ]; then
-    echo "ERROR: Not root user?"
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: This script must be run as root."
     exit 1
 fi
 
+read -p "Enter the service name (default: $service_name): " custom_service_name
+read -p "Enter the script location (default: $script_location): " custom_script_location
+
+# Use default values if user input is empty
+service_name="${custom_service_name:-$service_name}"
+script_location="${custom_script_location:-$script_location}"
+
 echo "Creating launch bash script..."
-# Create a bash script that would run jar file
-echo "#!/bin/sh" > ./network-server.sh
-echo "/usr/bin/java -jar network-server.jar" >> ./network-server.sh
-echo "" >> ./network-server.sh 
-chmod 777 ./network-server.sh
+
+# Create a bash script that runs the jar file
+cat > "$script_location" <<EOF
+#!/bin/bash
+/usr/bin/java -jar network-server.jar
+EOF
+
+chmod 755 "$script_location"
 
 # Install as a system service
-echo "Installing JAR application as a system service..."
-touch /lib/systemd/system/lorans.service
-echo "[Unit]" > /lib/systemd/system/lorans.service
-echo "Description=LoRa Network Server" >> /lib/systemd/system/lorans.service
-echo "[Service]" >> /lib/systemd/system/lorans.service
-echo "User=`echo $USER`" >> /lib/systemd/system/lorans.service
-echo "WorkingDirectory=`pwd`" >> /lib/systemd/system/lorans.service
-echo "#executable is a bash script which calls jar" >> /lib/systemd/system/lorans.service
-echo "ExecStart=`pwd`/network-server.sh" >> /lib/systemd/system/lorans.service
-echo "SuccessExitStatus=143" >> /lib/systemd/system/lorans.service
-echo "TimeoutStopSec=10" >> /lib/systemd/system/lorans.service
-echo "Restart=on-failure" >> /lib/systemd/system/lorans.service
-echo "RestartSec=5" >> /lib/systemd/system/lorans.service
-echo "[Install]" >> /lib/systemd/system/lorans.service
-echo "WantedBy=multi-user.target" >> /lib/systemd/system/lorans.service
-echo "" >> /lib/systemd/system/lorans.service
+service_file="/lib/systemd/system/$service_name.service"
 
-systemctl enable lorans.service
+echo "Installing JAR application as a system service..."
+cat > "$service_file" <<EOF
+[Unit]
+Description=LoRa Network Server
+
+[Service]
+User=$SUDO_USER
+WorkingDirectory=$(pwd)
+ExecStart=$script_location
+SuccessExitStatus=143
+TimeoutStopSec=10
+Restart=on-failure
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable "$service_name.service"
 echo "Everything done!"
 echo "Restarting systemctl"
 systemctl daemon-reload
-echo "Daemon is located at /lib/systemd/system/lorans.service" 
-echo "To disable this service, run this command as super user:" 
-echo "   systemctl disable lorans.service"
-
+echo "Daemon is located at $service_file"
+echo "To disable this service, run this command as super user:"
+echo "   systemctl disable $service_name.service"
