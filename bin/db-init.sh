0b1ef82999e1f6930b42a49da164b5b6cf403b2f
Alexander Valach <25947314+alexandervalach@users.noreply.github.com>
2023-11-04 13:01:57 +0100

    Update db-init.sh

--- bin/db-init.sh
+++ bin/db-init.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-docker cp ../db/db_tables_with_dummy_data.sql postgres_container:/
+# Set default values for database name and user
+DB_NAME="lones"
+DB_USER="lones"
 
-docker exec -it postgres_container bash -c 'psql -d lones -U lones -f db_tables_with_dummy_data.sql;'
+# Function to check if the PostgreSQL container is running
+is_postgres_container_running() {
+  if docker ps --format "{{.Names}}" | grep -q "postgres_container"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Check if the PostgreSQL container is running
+if is_postgres_container_running; then
+  # Copy SQL script to the PostgreSQL container
+  docker cp ./db/db_tables_with_dummy_data.sql postgres_container:/
+
+  # Execute the SQL script with the provided database name and user
+  docker exec -it postgres_container bash -c "psql -d $DB_NAME -U $DB_USER -f db_tables_with_dummy_data.sql"
+else
+  echo "PostgreSQL container is not running. Please start the PostgreSQL container first."
+fi
